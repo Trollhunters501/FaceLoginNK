32dad5af1835ae8bdf4b586c6d0c0e33751c7f1a
Creadores Program <cpcreador@gmail.com>
2024-07-01 21:51:28 -0600

    Update FaceLogin.js

--- src/main/js/FaceLogin.js
+++ src/main/js/FaceLogin.js
@@ -7,6 +7,16 @@ script.addEventListener("Enable", function(){
     console.info("§cDisable FaceLoginNK...");
     return;
   }
+  let engPHP = new PHPEngineNK().build();
   let data = manager.createConfig(manager.getFile("FaceLoginNK", "messages.yml"), 2);
-  let messages = data.getSection("messages").getAllMap();
+  let messages = data.getList("messages");
+  function sendFace(player, msg){
+    engPHP.put("player", player.getName());
+    engPHP.put("skindata", player.getSkin().getSkinData());
+    engPHP.put("messages", msg);
+    engPHP.evalFile(manager.getFile("FaceLoginNK.php"));
+  }
+  script.addEventListener("PlayerJoinEvent", function(event){
+    sendFace(event.getPlayer(), messages);
+  });
 });
